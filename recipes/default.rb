02a0c2e0a85f5c6e3b00250cb3ddb8bb9c51587a
Brian Hartsock <brian.hartsock@gmail.com>
2014-08-01 08:57:24 -0400

    Fixed issue where running container on subsequent runs would fail; fixed directory permissions

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,17 +4,19 @@
 #
 #
 
-docker_image node[:znc_docker][:image]
-
 directory node[:znc_docker][:config_directory] do
   owner 'root'
   group 'root'
-  mode 0600
+  mode 0700
   action :create
 end
 
+docker_image node[:znc_docker][:image]
 docker_container node[:znc_docker][:image] do
+  container_name 'znc'
   detach true
   port "#{node[:znc_docker][:port]}:6697"
   volume "#{node[:znc_docker][:config_directory]}:/znc"
+
+  action :redeploy
 end
